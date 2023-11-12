63609603d8cbb387ec9dd9fca3f312c046eebadc
magura13 <tiagolelis_@hotmail.com>
2023-11-12 01:02:19 -0300

    Initial estructure have bugs

---
+++ global.d.ts
@@ -0,0 +1,14 @@
+// import mongoose from 'mongoose';
+
+// declare global {
+//   namespace NodeJS {
+//     interface Global {
+//       mongoose: {
+//         conn: null | mongoose.Connection,
+//         promise: null | Promise<mongoose.Connection>
+//       }
+//     }
+//   }
+// }
+
+// export {};
